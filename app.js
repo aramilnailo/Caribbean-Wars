2c75578e5256e66a171f8e188634826642c8548b
Thomas Judge <jtjudge@iastate.edu>
2017-02-05 15:01:26 -0600

    Added basic socket.io interaction.

---
+++ app.js
@@ -0,0 +1,25 @@
+
+var express = require("express");
+var app = express();
+var serv = require("http").Server(app);
+
+app.get("/", function(req, res) {
+	res.sendFile(__dirname + "/client/index.html");
+});
+app.use("/client", express.static(__dirname + "/client"));
+
+serv.listen(2000);
+console.log("Server started");
+
+var io = require("socket.io")(serv, {});
+io.sockets.on("connection", function(socket) {
+	console.log("Socket connection");
+	
+	socket.on("message", function(data) {
+		console.log("Client message: " + data.message);
+		
+		socket.emit("message", {
+			message:"happy birthday!"
+		});
+	});
+});
