88e36ea6efea56f5c3defea122c1550844f90622
Thomas Judge <jtjudge@iastate.edu>
2017-02-05 15:37:49 -0600

    Can now demonstrate synch between multiple visitors.

--- app.js
+++ app.js
@@ -11,15 +11,39 @@ app.use("/client", express.static(__dirname + "/client"));
 serv.listen(2000);
 console.log("Server started");
 
+var SOCKET_LIST = {};
+
 var io = require("socket.io")(serv, {});
 io.sockets.on("connection", function(socket) {
-	console.log("Socket connection");
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	socket.number = "" + Math.floor(10 * Math.random());
 	
-	socket.on("message", function(data) {
-		console.log("Client message: " + data.message);
-		
-		socket.emit("message", {
-			message:"happy birthday!"
-		});
+	SOCKET_LIST[socket.id] = socket;
+	
+	socket.on("disconnect", function() {
+		delete SOCKET_LIST[socket.id];
 	});
 });
+
+setInterval(function() {
+	var pack = [];
+	for(var i in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			number:socket.number
+		});
+	}
+	for(var i in SOCKET_LIST) {
+		var socket = SOCKET_LIST[i];
+		socket.emit("newPositions", pack);
+	}
+}, 1000/25);
+
+
+
