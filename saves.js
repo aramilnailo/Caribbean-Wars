dda1a0355304bf2e68b29964a6cb8794c4183b5c
jtjudge <jtjudge@iastate.edu>
2017-02-28 01:08:19 -0600

    Fixed the bugs, added missing functionality.

---
+++ saves.js
@@ -0,0 +1,43 @@
+var debug = require("./debug.js").saves;
+var log = require("./debug.js").log;
+
+var dbi = require("./dbi.js");
+	
+var Saves = function() {}
+
+Saves.prototype.listen = function(sox) {
+	sox.listen("deleteSavedGame", this.deleteSavedGame);
+	sox.listen("saveGameRequest", this.saveGameRequest);
+	sox.listen("savedGamesListRequest", this.savedGamesListRequest);
+}
+
+Saves.prototype.deleteSavedGame = function(param) {
+    var client = param.client;
+    var data = param.data;
+	dbi.removeSavedGame({file_name:data, author:client.player.username},
+		function(resp) {
+        var msg = resp ? "Deleted \"" + data + "\"." : 
+                    "Could not delete \"" + data + "\".";
+        client.socket.emit("alert", msg);
+    });
+}
+
+Saves.prototype.saveGameRequest = function(param) {
+    var client = param.client;
+    var data = param.data;
+	dbi.saveGameFilename(data, function(resp) {
+        var msg = resp ? "Saved \"" + data.file_name + "\"." :
+                        "Could not save \"" + data.file_name + "\".";
+	       client.socket.emit("alert", msg);
+    });
+}
+
+Saves.prototype.savedGamesListRequest = function(param) {
+    var client = param.client;
+    var data = param.data;
+	dbi.getSavedGamesList(function(data) {
+	    client.socket.emit("savedGamesListResponse", data);
+	});
+}
+
+module.exports = new Saves();
